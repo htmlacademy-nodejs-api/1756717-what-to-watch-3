c244baa7adc1a5c78869d1a5b75f00b74666e11c
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-12-22 17:48:29 +0300

    Раскрашивает вывод информации в консоли

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,21 +1,22 @@
 import { CliCommandInterface } from './cli-command.interface.js';
+import chalk from 'chalk';
 
 export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
 
   public async execute(): Promise<void> {
     console.log(`
-      Программа для подготовки данных для REST API сервера.
+      ${chalk.yellow('Программа для подготовки данных для REST API сервера.')}
 
-      Пример:
-          cli.js --<command> [--arguments]
+      ${chalk.yellow('Пример:')}
+          ${chalk.green.bold('cli.js --<command> [--arguments]')}
 
-      Команды:
+      ${chalk.yellow('Команды:')}
 
-          --version:                   # выводит номер версии
-          --help:                      # печатает этот текст
-          --import <path>:             # импортирует данные из TSV
-          --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+          ${chalk.green.bold('--version:')}                   ${chalk.blue('# выводит номер версии')}
+          ${chalk.green.bold('--help:')}                      ${chalk.blue('# печатает этот текст')}
+          ${chalk.green.bold('--import <path>:')}             ${chalk.blue('# импортирует данные из TSV')}
+          ${chalk.green.bold('--generate <n> <path> <url>')}  ${chalk.blue('# генерирует произвольное количество тестовых данных')}
     `);
   }
 }
