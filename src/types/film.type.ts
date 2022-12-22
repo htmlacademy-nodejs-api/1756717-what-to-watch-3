5cacd728946b6fb6ef7f2620c1d93dc4fef147c1
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-12-22 16:13:04 +0300

    Добавляет типы для импортируемых сущностей

---
+++ src/types/film.type.ts
@@ -0,0 +1,21 @@
+import { User } from './user.type.js';
+import { GenreType } from './genre-type.enum.js';
+
+export type Film = {
+  name: string;
+  description: string;
+  postDate: Date;
+  genre: GenreType;
+  released: number;
+  rating: number;
+  previewVideoLink: string;
+  videoLink: string;
+  starring: string[];
+  director: string;
+  runTime: number;
+  commentsAmount: number;
+  user: User;
+  posterImage: string;
+  backgroundImage: string;
+  backgroundColor: string
+}
