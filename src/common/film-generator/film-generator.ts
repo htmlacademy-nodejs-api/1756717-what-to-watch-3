83e97cc64c661e996c9eb08188a3889226174dcb
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-12-25 14:41:06 +0300

    Добавляет генератор объявлений

---
+++ src/common/film-generator/film-generator.ts
@@ -0,0 +1,42 @@
+import { MockData } from '../../types/mock-data.type.js';
+import { GenreType } from '../../types/genre-type.enum.js';
+import { generateRandomValue, getRandomItem, getRandomItems } from '../../utils/random.js';
+import { FilmGeneratorInterface } from './film-generator.interface.js';
+import { FilmReleaseYearSettings, RatingSettings, RunTimeSettings, CommentAmountSettings, DateSettings } from '../../utils/random-settings.js';
+import dayjs from 'dayjs';
+
+
+export default class FilmGenerator implements FilmGeneratorInterface {
+  constructor(private readonly mockData: MockData) {}
+
+  public generate(): string {
+    const name = getRandomItem<string>(this.mockData.names);
+    const description = getRandomItem<string>(this.mockData.descriptions);
+    const postDate = dayjs().subtract(generateRandomValue(DateSettings.FIRST_WEEK_DAY, DateSettings.LAST_WEEK_DAY), 'day').toISOString();
+    const genre = getRandomItem<string>(Object.keys(GenreType));
+    const released = generateRandomValue(FilmReleaseYearSettings.MIN, FilmReleaseYearSettings.MAX).toString();
+    const rating = generateRandomValue(RatingSettings.MIN, RatingSettings.MAX, RatingSettings.NUM_AFTER_DIGIT).toString();
+    const previewVideoLink = getRandomItem<string>(this.mockData.previewVideoLinks);
+    const videoLink = getRandomItem<string>(this.mockData.videoLinks);
+    const starring = getRandomItems<string>(this.mockData.actors).join(', ');
+    const director = getRandomItem<string>(this.mockData.directors);
+    const runTime = generateRandomValue(RunTimeSettings.MIN, RunTimeSettings.MAX).toString();
+    const commentsAmount = generateRandomValue(CommentAmountSettings.MIN, CommentAmountSettings.MAX).toString();
+    const userName = getRandomItem<string>(this.mockData.users);
+    const email = getRandomItem<string>(this.mockData.emails);
+    const avatarUrl = getRandomItem<string>(this.mockData.avatarUrls);
+    const posterImage = getRandomItem<string>(this.mockData.posterImages);
+    const backgroundImage = getRandomItem<string>(this.mockData.backgroundImages);
+    const backgroundColor = getRandomItem<string>(this.mockData.backgroundColors);
+
+    return [
+      name, description, postDate,
+      genre, released, rating,
+      previewVideoLink, videoLink,
+      starring, director, runTime,
+      commentsAmount, userName,
+      email, avatarUrl, posterImage,
+      backgroundImage, backgroundColor,
+    ].join('\t');
+  }
+}
---
+++ src/utils/random-settings.ts
@@ -0,0 +1,25 @@
+export const FilmReleaseYearSettings = {
+  MIN: 1990,
+  MAX: 2020,
+};
+
+export const RatingSettings = {
+  MIN: 0,
+  MAX: 10,
+  NUM_AFTER_DIGIT: 1,
+};
+
+export const RunTimeSettings = {
+  MIN: 90,
+  MAX: 220,
+};
+
+export const CommentAmountSettings = {
+  MIN: 0,
+  MAX: 100,
+};
+
+export const DateSettings = {
+  FIRST_WEEK_DAY: 1,
+  LAST_WEEK_DAY: 7,
+};
