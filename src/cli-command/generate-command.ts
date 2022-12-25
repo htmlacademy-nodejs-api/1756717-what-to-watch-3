0c0c0339b7d4ac095fefdfaa54eaad90e40971cf
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-12-25 14:50:26 +0300

    Добавляет команду формирования объявлений

---
+++ src/cli-command/generate-command.ts
@@ -0,0 +1,12 @@
+import { MockData } from '../types/mock-data.type.js';
+import { CliCommandInterface } from './cli-command.interface.js';
+
+export default class GenerateCommand implements CliCommandInterface {
+  public readonly name = '--generate';
+  private initialData!: MockData;
+
+  public async execute(...parameters: string[]): Promise<void> {
+    const [count, filepath, url] = parameters;
+    const filmCount = Number.parseInt(count, 10);
+  }
+}
