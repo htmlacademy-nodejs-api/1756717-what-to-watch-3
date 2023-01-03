7cc86d99b89e82f7723b679c58d7cd6756caa42d
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-01-03 16:05:54 +0300

    Добавляет поддержку dotenv в ConfigService

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,10 +1,22 @@
 import { ConfigInterface } from './config.interface.js';
+import { config, DotenvParseOutput } from 'dotenv';
+import { LoggerInterface } from '../logger/logger.interface.js';
 
 export default class ConfigService implements ConfigInterface {
-  private config: NodeJS.ProcessEnv;
+  private config: DotenvParseOutput;
+  private logger: LoggerInterface;
 
-  constructor() {
-    this.config = process.env;
+  constructor(logger: LoggerInterface) {
+    this.logger = logger;
+
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exist.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env file found and succesfully parsed!');
   }
 
   public get(key: string): string | undefined {
