5e0412f24510a9e3c1ee7b9272b98a28ef06417d
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-01-03 16:30:14 +0300

    Добавляет ConfigService в Application

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,9 +1,11 @@
 import { LoggerInterface } from '../common/logger/logger.interface.js';
+import { ConfigInterface } from '../common/config/config.interface.js';
 
 export default class Application {
-  constructor(private logger: LoggerInterface) {}
+  constructor(private logger: LoggerInterface, private config: ConfigInterface) {}
 
   public async init() {
     this.logger.info('Application initialization...');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
