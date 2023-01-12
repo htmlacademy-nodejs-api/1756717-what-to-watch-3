fc3c85ba9cabcaaad4e051a0c88f78f3d7da8a4a
Mariya <Elistratova.Mari.22@yandex.ru>
2023-01-12 23:47:51 +0300

    Добавляет методы в модель UserModel

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import { GenreType } from '../types/genre-type.enum.js';
 import { Film } from '../types/film.type.js';
 
@@ -47,3 +48,8 @@ export const createFilm = (row: string) => {
 
 export const getErrorMessage = (error: unknown): string =>
   error instanceof Error ? error.message : '';
+
+export const createSHA256 = (line: string, salt: string): string => {
+  const shaHasher = crypto.createHmac('sha256', salt);
+  return shaHasher.update(line).digest('hex');
+};
