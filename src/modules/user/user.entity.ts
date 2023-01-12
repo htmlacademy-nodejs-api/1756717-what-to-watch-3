8dd5bcf1b0a2252e6f9c5bbeda8c8ceac9b2e091
Mariya <Elistratova.Mari.22@yandex.ru>
2023-01-12 23:19:09 +0300

    Добавляет описание схемы

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,7 +1,20 @@
 import { User } from '../../types/user.type.js';
+import typegoose, { getModelForClass, defaultClasses } from '@typegoose/typegoose';
 
-export class UserEntity implements User {
+const { prop } = typegoose;
+
+export class UserEntity extends defaultClasses.TimeStamps implements User {
+  @prop({ required: true })
   public userName!: string;
+
+  @prop({ unique: true, required: true })
   public email!: string;
+
+  @prop()
   public avatarUrl!: string;
+
+  @prop({ required: true })
+  public password!: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
