f17da6b59ba61771b1867c02fc3a2762869dd860
Mariya <Elistratova.Mari.22@yandex.ru>
2023-01-13 00:59:30 +0300

    Добавляет внедрение зависимостей в UserService

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { Container } from 'inversify';
 import { applicationContainer } from './app/application.container.js';
 import Application from './app/application.js';
 import { Component } from './types/component.types.js';
+import { userContainer } from './modules/user/user.container.js';
 
 async function bootstrap() {
-  const mainContainer = Container.merge(applicationContainer);
+  const mainContainer = Container.merge(
+    applicationContainer,
+    userContainer
+);
   const application = mainContainer.get<Application>(Component.Application);
   await application.init();
 }
