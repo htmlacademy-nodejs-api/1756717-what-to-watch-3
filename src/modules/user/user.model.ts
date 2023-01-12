30a47138b0bc5ba5d27bb8ddbc1763d5e1a33939
Mariya <Elistratova.Mari.22@yandex.ru>
2023-01-12 22:08:38 +0300

    Расширяет интерфейс UserDocument(timestamp)

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 import { User } from '../../types/user.type.js';
 
-export interface UserDocument extends User, mongoose.Document {}
+export interface UserDocument extends User, mongoose.Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new mongoose.Schema({
   userName: String,
   email: String,
   avatarUrl: String,
+}, {
+  timestamps: true,
 });
 
 export const UserModel = mongoose.model<UserDocument>('User', userSchema);
