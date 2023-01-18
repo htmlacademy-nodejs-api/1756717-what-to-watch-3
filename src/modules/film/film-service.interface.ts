7ed52203a7e3cec18e6798870716c921c068e5a9
Mariya <Elistratova.Mari.22@yandex.ru>
2023-01-18 22:37:34 +0300

    Расширяет интерфейс OfferService

--- src/modules/film/film-service.interface.ts
+++ src/modules/film/film-service.interface.ts
@@ -1,8 +1,18 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { FilmEntity } from './film.entity.js';
 import CreateFilmDto from './dto/create-film.dto.js';
+import UpdateFilmDto from './dto/update-film.dto.js';
 
 export interface FilmServiceInterface {
   create(dto: CreateFilmDto): Promise<DocumentType<FilmEntity>>;
   findById(filmId: string): Promise<DocumentType<FilmEntity> | null>;
+  find(count?: number): Promise<DocumentType<FilmEntity>[]>;
+  deleteById(filmId: string): Promise<DocumentType<FilmEntity> | null>;
+  updateById(filmId: string, dto: UpdateFilmDto): Promise<DocumentType<FilmEntity> | null>;
+  findByGenre(genre: string, count?: number): Promise<DocumentType<FilmEntity>[]>;
+  incCommentCount(filmId: string): Promise<DocumentType<FilmEntity> | null>;
+  findPromo(): Promise<DocumentType<FilmEntity>>;
+  findFavorite(): Promise<DocumentType<FilmEntity>[]>
+  addOrDeleteFavorite(filmId: string): Promise<DocumentType<FilmEntity>>;
+  exists(documentId: string): Promise<boolean>;
 }
