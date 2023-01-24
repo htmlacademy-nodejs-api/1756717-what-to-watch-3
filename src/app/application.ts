320cbaca8bce7b7416d3f6d280173d8dffcaf522
Mariya <Elistratova.Mari.22@yandex.ru>
2023-01-25 00:37:58 +0300

    Добавляет метод initMiddleware

--- src/app/application.ts
+++ src/app/application.ts
@@ -24,6 +24,10 @@ export default class Application {
     this.expressApp.use('/films', this.filmController.router);
   }
 
+  public initMiddleware() {
+    this.expressApp.use(express.json());
+  }
+
   public async init() {
     this.logger.info('Application initialization...');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
@@ -38,6 +42,7 @@ export default class Application {
 
     await this.databaseClient.connect(uri);
 
+    this.initMiddleware();
     this.initRoutes();
     this.expressApp.listen(this.config.get('PORT'));
     this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
