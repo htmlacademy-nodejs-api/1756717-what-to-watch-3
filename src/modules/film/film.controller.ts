186f4623aef956db9e443e435759816527c27e4d
Mariya <Elistratova.Mari.22@yandex.ru>
2023-01-26 03:08:09 +0300

    Добавляет limit в метод index FilmController

--- src/modules/film/film.controller.ts
+++ src/modules/film/film.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { inject, injectable } from 'inversify';
+import * as core from 'express-serve-static-core';
 import { Controller } from '../../common/controller/controller.js';
 import { Component } from '../../types/component.types.js';
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
@@ -10,6 +11,7 @@ import { StatusCodes } from 'http-status-codes';
 import { fillDTO } from '../../utils/common.js';
 import CreateFilmDto from './dto/create-film.dto.js';
 import HttpError from '../../common/errors/http-error.js';
+import { RequestQuery } from '../../types/request-query.type.js';
 
 @injectable()
 export default class FilmController extends Controller {
@@ -25,11 +27,15 @@ export default class FilmController extends Controller {
     this.addRoute({path: '/', method: HttpMethod.Post, handler: this.create});
   }
 
-  public async index(_req: Request, res: Response): Promise<void> {
-    const films = await this.filmService.find();
-    const filmResponse = fillDTO(FilmResponse, films);
-    this.ok(res, filmResponse);
-  }
+  public async index(
+    req: Request<core.ParamsDictionary, unknown, unknown, RequestQuery>,
+    res: Response
+  ): Promise<void> {
+      const limit = req.query?.limit;
+      const films = await this.filmService.find(limit);
+      const filmResponse = fillDTO(FilmResponse, films);
+      this.ok(res, filmResponse);
+    }
 
   public async create(
     {body}: Request<Record<string, unknown>, Record<string, unknown>, CreateFilmDto>,
