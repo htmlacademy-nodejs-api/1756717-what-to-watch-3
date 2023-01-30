f668231b08d86a68a2eebc8c5836bf266201a716
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-01-30 20:03:55 +0300

    Добавляет валидацию для CreateUserDto

---
+++ src/modules/user/user.constant.ts
@@ -0,0 +1,13 @@
+export const USERNAME_VALIDATION_MESSAGE = 'Min length is 1, max length is 15';
+
+export const USERNAME_REQUIRED_MESSAGE = 'userName is required';
+
+export const EMAIL_REQUIRED_MESSAGE = 'email is required';
+
+export const EMAIL_VALIDATION_MESSAGE = 'email must be valid address';
+
+export const AVATAR_VALIDATION_MESSAGE = 'avatarUrl field must be a link on .jpg or .png format';
+
+export const PASSWORD_VALIDATION_MESSAGE = 'Min length is 6, max length is 12';
+
+export const PASSWORD_REQUIRED_MESSAGE = 'password is required';
