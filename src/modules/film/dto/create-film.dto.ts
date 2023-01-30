143afd0ccb58fee17b97b617482c3221e0840c3d
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-01-30 17:21:33 +0300

    Добавляет валидаторы для CreateFilmDto

--- src/modules/film/dto/create-film.dto.ts
+++ src/modules/film/dto/create-film.dto.ts
@@ -1,19 +1,41 @@
+import { Length, IsDateString, IsEnum, IsArray, IsMongoId, Contains } from 'class-validator';
 import { GenreType } from '../../../types/genre-type.enum.js';
+import { BACKGROUNDIMAGE_VALIDATION_MESSAGE, DESCRIPTION_VALIDATION_MESSAGE, DIRECTOR_VALIDATION_MESSAGE, GENRE_VALIDATION_MESSAGE, NAME_VALIDATION_MESSAGE, POSTDATE_VALIDATION_MESSAGE, POSTER_VALIDATION_MESSAGE, STARRING_VALIDATION_MESSAGE, USER_VALIDATION_MESSAGE } from '../film.constant.js';
 
 export default class CreateFilmDto {
+  @Length(2, 100, {message: NAME_VALIDATION_MESSAGE})
   public name!: string;
+
+  @Length(20, 1024, {message: DESCRIPTION_VALIDATION_MESSAGE})
   public description!: string;
+
+  @IsDateString({}, {message: POSTDATE_VALIDATION_MESSAGE})
   public postDate!: Date;
+
+  @IsEnum(GenreType, {message: GENRE_VALIDATION_MESSAGE})
   public genre!: GenreType;
+
   public released!: number;
   public rating!: number;
   public previewVideoLink!: string;
   public videoLink!: string;
+
+  @IsArray({message: STARRING_VALIDATION_MESSAGE})
   public starring!: string[];
+
+  @Length(2, 50, {message: DIRECTOR_VALIDATION_MESSAGE})
   public director!: string;
+
   public runTime!: number;
+
+  @IsMongoId({message: USER_VALIDATION_MESSAGE})
   public userId!: string;
+
+  @Contains('.jpg', {message: POSTER_VALIDATION_MESSAGE})
   public posterImage!: string;
+
+  @Contains('.jpg', {message: BACKGROUNDIMAGE_VALIDATION_MESSAGE})
   public backgroundImage!: string;
+
   public backgroundColor!: string;
 }
--- src/modules/film/film.constant.ts
+++ src/modules/film/film.constant.ts
@@ -1,1 +1,19 @@
 export const DEFAULT_FILM_COUNT = 60;
+
+export const NAME_VALIDATION_MESSAGE = 'Min length is 2, max length is 100';
+
+export const DESCRIPTION_VALIDATION_MESSAGE = 'Min length is 20, max length is 1024';
+
+export const POSTDATE_VALIDATION_MESSAGE = 'postDate must be valid ISO date';
+
+export const GENRE_VALIDATION_MESSAGE = 'Genre must be comedy, crime, documentary, drama, horror, family, romance, scifi or thriller';
+
+export const STARRING_VALIDATION_MESSAGE = 'Field starring must be an array';
+
+export const DIRECTOR_VALIDATION_MESSAGE = 'Min length is 2, max length is 50';
+
+export const USER_VALIDATION_MESSAGE = 'userId field must be a valid id';
+
+export const POSTER_VALIDATION_MESSAGE = 'posterImage field must be a link on .jpg format';
+
+export const BACKGROUNDIMAGE_VALIDATION_MESSAGE = 'backgroundImage field must be a link on .jpg format';
