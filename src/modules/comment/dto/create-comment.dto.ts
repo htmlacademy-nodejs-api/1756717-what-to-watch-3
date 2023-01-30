ca122590adf63ec640bdc54f6a03970a0c6a5673
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-01-30 21:16:33 +0300

    Добавляет валидацию в CreateCommentDto

--- src/modules/comment/dto/create-comment.dto.ts
+++ src/modules/comment/dto/create-comment.dto.ts
@@ -1,7 +1,21 @@
+import { IsString, IsMongoId, Length, Max, Min, IsDateString } from 'class-validator';
+import { FILMID_VALIDATION_MESSAGE, MAX_RATING_VALIDATION_MESSAGE, MESSAGE_REQUIRED_MESSAGE, MESSAGE_VALIDATION_MESSAGE, MIN_RATING_VALIDATION_MESSAGE, POSTDATE_COMMENT_VALIDATION_MESSAGE, USERID_VALIDATION_MESSAGE } from '../comment.constant.js';
+
 export default class CreateCommentDto {
+  @IsString({message: MESSAGE_REQUIRED_MESSAGE})
+  @Length(5, 1024, {message: MESSAGE_VALIDATION_MESSAGE})
   public message!: string;
+
+  @Min(1, {message: MIN_RATING_VALIDATION_MESSAGE})
+  @Max(10, {message: MAX_RATING_VALIDATION_MESSAGE})
   public rating!: number;
+
+  @IsDateString({}, {message: POSTDATE_COMMENT_VALIDATION_MESSAGE})
   public postDate!: Date;
+
+  @IsMongoId({message: USERID_VALIDATION_MESSAGE})
   public userId!: string;
+
+  @IsMongoId({message: FILMID_VALIDATION_MESSAGE})
   public filmId!: string;
 }
